7a3fb3ba0f05caadbdcf1f29fbabbe5a06794f9c
agaurav546 <agaurav546@gmail.com>
2023-10-16 22:54:21 +0530

    project files

---
+++ Assignment13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment13
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example usage of the sum method
+            int result = sum(5, 3);
+            Console.WriteLine("The sum is: " + result);
+        }
+
+        // Method that returns the sum of two integers
+        public static int sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Assignment13Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using Assignment13;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using Assert = NUnit.Framework.Assert;
+
+namespace Assignment13Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestSum()
+        {
+            // Arrange
+            int a = 5;
+            int b = 3;
+            int expectedSum = 8;
+
+            // Act
+            int actualSum = Program.sum(a, b);
+
+            // Assert
+            Assert.AreEqual(expectedSum, actualSum, "Sum of a and b is not as expected.");
+        }
+    }
+}
+
+
